7af65057a4957484a59287921de0a4d69ed37543
jaypassley <jaypassley@yahoo.com>
2022-03-09 12:40:48 -0500

    Add project files.

---
+++ ResumeApp/authentication_form.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResumeApp
+{
+    public partial class authentication_form : Form
+    {
+        public authentication_form()
+        {
+            InitializeComponent();
+        }
+
+        private void authentication_form_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ResumeApp/info_submit_form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResumeApp
+{
+    public partial class info_submit_form : Form
+    {
+        public info_submit_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
