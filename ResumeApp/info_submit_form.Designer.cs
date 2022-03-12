02fee87a3d47bf9b246266c3fa21331603caba00
OrieRobisnon <robinson.orie@yahoo.com>
2022-03-12 12:09:05 -0500

    Changes again

--- ResumeApp/info_submit_form.Designer.cs
+++ ResumeApp/info_submit_form.Designer.cs
@@ -29,7 +29,22 @@
         private void InitializeComponent()
         {
             this.lb_adv_tech_heading = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.FNAME = new System.Windows.Forms.Label();
+            this.Index = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.mdname = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Lname = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.dob = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.QF = new System.Windows.Forms.Label();
+            this.WEXP = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // lb_adv_tech_heading
@@ -37,30 +52,166 @@
             this.lb_adv_tech_heading.AutoSize = true;
             this.lb_adv_tech_heading.Font = new System.Drawing.Font("Times New Roman", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lb_adv_tech_heading.ForeColor = System.Drawing.Color.Maroon;
-            this.lb_adv_tech_heading.Location = new System.Drawing.Point(53, 9);
+            this.lb_adv_tech_heading.Location = new System.Drawing.Point(43, 36);
             this.lb_adv_tech_heading.Name = "lb_adv_tech_heading";
             this.lb_adv_tech_heading.Size = new System.Drawing.Size(616, 55);
             this.lb_adv_tech_heading.TabIndex = 1;
             this.lb_adv_tech_heading.Text = "Advance Technology Experts";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 149);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // FNAME
+            // 
+            this.FNAME.AutoSize = true;
+            this.FNAME.Location = new System.Drawing.Point(53, 133);
+            this.FNAME.Name = "FNAME";
+            this.FNAME.Size = new System.Drawing.Size(60, 13);
+            this.FNAME.TabIndex = 3;
+            this.FNAME.Text = "First Name ";
+            this.FNAME.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Index
+            // 
+            this.Index.AutoSize = true;
+            this.Index.Location = new System.Drawing.Point(53, 185);
+            this.Index.Name = "Index";
+            this.Index.Size = new System.Drawing.Size(49, 13);
+            this.Index.TabIndex = 3;
+            this.Index.Text = "Index # :";
+            this.Index.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(278, 149);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(153, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // mdname
+            // 
+            this.mdname.AutoSize = true;
+            this.mdname.Location = new System.Drawing.Point(275, 133);
+            this.mdname.Name = "mdname";
+            this.mdname.Size = new System.Drawing.Size(72, 13);
+            this.mdname.TabIndex = 3;
+            this.mdname.Text = "Middle Name ";
+            this.mdname.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(506, 149);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(153, 20);
+            this.textBox4.TabIndex = 2;
+            // 
+            // Lname
+            // 
+            this.Lname.AutoSize = true;
+            this.Lname.Location = new System.Drawing.Point(503, 133);
+            this.Lname.Name = "Lname";
+            this.Lname.Size = new System.Drawing.Size(61, 13);
+            this.Lname.TabIndex = 3;
+            this.Lname.Text = "Last Name ";
+            this.Lname.Click += new System.EventHandler(this.label1_Click);
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(60, 115);
+            this.label1.Location = new System.Drawing.Point(53, 235);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Full Name";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Index # :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dob
+            // 
+            this.dob.AutoSize = true;
+            this.dob.Location = new System.Drawing.Point(50, 235);
+            this.dob.Name = "dob";
+            this.dob.Size = new System.Drawing.Size(69, 13);
+            this.dob.TabIndex = 3;
+            this.dob.Text = "Dath of Birth ";
+            this.dob.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(53, 251);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(56, 202);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(278, 201);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(153, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(506, 202);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(153, 20);
+            this.textBox5.TabIndex = 2;
+            // 
+            // QF
+            // 
+            this.QF.AutoSize = true;
+            this.QF.Location = new System.Drawing.Point(275, 185);
+            this.QF.Name = "QF";
+            this.QF.Size = new System.Drawing.Size(118, 13);
+            this.QF.TabIndex = 3;
+            this.QF.Text = "Academic Qualification ";
+            this.QF.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // WEXP
+            // 
+            this.WEXP.AutoSize = true;
+            this.WEXP.Location = new System.Drawing.Point(503, 186);
+            this.WEXP.Name = "WEXP";
+            this.WEXP.Size = new System.Drawing.Size(89, 13);
+            this.WEXP.TabIndex = 3;
+            this.WEXP.Text = "Work Experience";
+            this.WEXP.Click += new System.EventHandler(this.label1_Click);
             // 
             // info_submit_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(716, 448);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dob);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.Index);
+            this.Controls.Add(this.WEXP);
+            this.Controls.Add(this.Lname);
+            this.Controls.Add(this.QF);
+            this.Controls.Add(this.mdname);
+            this.Controls.Add(this.FNAME);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.lb_adv_tech_heading);
             this.Name = "info_submit_form";
             this.Text = "Info_Submission_Form";
+            this.Load += new System.EventHandler(this.info_submit_form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,6 +220,20 @@
         #endregion
 
         private System.Windows.Forms.Label lb_adv_tech_heading;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label FNAME;
+        private System.Windows.Forms.Label Index;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label mdname;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label Lname;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label dob;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label QF;
+        private System.Windows.Forms.Label WEXP;
     }
 }
--- ResumeApp/info_submit_form.cs
+++ ResumeApp/info_submit_form.cs
@@ -16,5 +16,15 @@ namespace ResumeApp
         {
             InitializeComponent();
         }
+
+        private void info_submit_form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
